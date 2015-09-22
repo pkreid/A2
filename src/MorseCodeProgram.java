cc266e1ea98b8200c82076ef5cc6c5e71baacb2a
Peter Kowalchuk-Reid <peter.kowalchuk.reid@gmail.com>
2015-09-22 12:15:17 -0400

    Create Project, add GetCode method

---
+++ src/MorseCodeProgram.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+
+/**
+ * @author Peter Kowalchuk-Reid
+ * Student ID: 101006196
+ *
+ */
+public class MorseCodeProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static String GetMorse(char letter){
+		String code;
+		switch (letter) {
+		case 'A': code = ".--";  break;
+		case 'B': code = "-..."; break;
+		case 'C': code = "-.-."; break;
+		case 'D': code = "-..";  break;
+		case 'E': code = ".";    break;
+		case 'F': code = "..-."; break;
+		case 'G': code = "--.";  break;
+		case 'H': code = "...."; break;
+		case 'I': code = "..";   break;
+		case 'J': code = ".---"; break;
+		case 'K': code = "-.-";  break;
+		case 'L': code = ".-.."; break;
+		case 'M': code = "--";   break;
+		case 'O': code = "---";  break;
+		case 'P': code = ".--."; break;
+		case 'Q': code = "--.-"; break;
+		case 'R': code = ".-.";  break;
+		case 'S': code = "...";  break;
+		case 'T': code = "-";    break;
+		case 'U': code = "..-";  break;
+		case 'V': code = "...-"; break;
+		case 'W': code = ".--";  break;
+		case 'X': code = "-.--"; break;
+		case 'Y': code = "-.--"; break;
+		case 'Z': code = "--.."; break;
+		case ' ': code = " ";    break;
+		}
+		return code;
+
+
+
+
+		}
+		return "FOO";
+
+	}
+}
+
+
+:
