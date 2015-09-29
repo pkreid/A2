1baf6752a896b1545062b8f90c9bf1e31bcd6b8d
Peter Kowalchuk-Reid <peter.kowalchuk.reid@gmail.com>
2015-09-29 19:07:55 -0400

    Assignment requires hard coding of values

--- ReverseMorseCodeProgram/CellPhoneProgram.java
+++ ReverseMorseCodeProgram/CellPhoneProgram.java
@@ -4,8 +4,17 @@ public class CellPhoneProgram {
 	/**
 	 * @param args
 	 */
+	static final int city1x = 100;
+	static final int city1y = 360;
+	static final int city2x = 250;
+	static final int city2y = 360;
+	static final int city3x = 340;
+	static final int city3y = 250;
+	static final int city4x = 230;
+	static final int city4y = 140;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
 
